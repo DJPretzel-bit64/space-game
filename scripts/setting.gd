0090fdc2bf842b96acbd5e64ff876c72de5ce1b4
p2mugabi <philipmugabi@gmail.com>
2025-11-08 13:01:52 -0800

    Created Main Menu and Audio Player Scenes

--- scripts/setting.gd
+++ scripts/setting.gd
@@ -1,10 +1,32 @@
 extends Control
 
+@onready var option_button = $MarginContainer/VBoxContainer/Resolution as OptionButton
+
+const WINDOW_MODE_ARRAY: Array[String] = [
+	"Full-Screen",
+	"Windowed",
+	"Borderless Window",
+	"Borderless Full-Screen"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	option_button.item_selected.connect(on_window_mode_selected)
+	
+func on_window_mode_selected(index: int) -> void:
+	match index:
+		0: #Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		1: #Windowed
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		2: #Borderless Window
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		3: #Borderless Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,12 +39,5 @@ func _on_volume_value_changed(value: float) -> void:
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0, toggled_on)
 
-
-func _on_resolution_item_selected(index: int) -> void:
-	match index:
-		0:
-			DisplayServer.window_set_size(Vector2(1920, 1080))
-		1:
-			DisplayServer.window_set_size(Vector2i(1200, 900))
-		2:
-			DisplayServer.window_set_size(Vector2i(1080, 720))
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
