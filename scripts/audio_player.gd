47d05c3c500acaa6dd23820a97f30640dfade8d6
p2mugabi <philipmugabi@gmail.com>
2025-11-08 20:14:15 -0800

    Changed the music options

--- scripts/audio_player.gd
+++ scripts/audio_player.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
-const level_music = preload("res://Music/Tame Impala - Dracula (Lyrics) - TrendingTracks.mp3")
+const level_music = preload("res://Music/boom-baddie-konstantin-garbuzyuk-main-version-42706-01-45.mp3")
 
 func _play_music(music: AudioStream):
 	if stream == music:
