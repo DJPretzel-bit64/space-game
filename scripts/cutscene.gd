3d0f45efab18f43cca765607b5ad4d6df7bc5e92
Dylan Hetzel <dylanjhetzel@student.cofo.edu>
2025-11-09 03:47:29 -0600

    made it so you can click to skip cutscene

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _process(_delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		_on_video_stream_player_finished()
+
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
